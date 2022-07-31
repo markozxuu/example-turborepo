d00f3bcd8df7b02b0bcc1fb8239ec5c0d6838841
Markoz Peña Mendez <markdrew53@gmail.com>
2022-07-31 16:41:28 -0500

    feat: 🎸 :Ddkdnkdnkwndknwkdwnkd

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docs!! :D</h1>
+      <h1>Docs with Evelyn</h1>
       <Button title="Test click" />
     </div>
   );
