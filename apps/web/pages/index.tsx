b5e0d2b425f7fe775fb2d0b572977f06c490458c
Markoz Peña Mendez <markdrew53@gmail.com>
2022-07-31 17:27:43 -0500

    feat: 🎸 new changes to ci

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web ! 2.3</h1>
       <Button title="click max" />
     </div>
   );
